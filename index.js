19b2321e84939ae10636a3b0ecc845cd5aa8b8d8
Aaron Sanchez <saarong12@gmail.com>
2024-01-05 22:00:53 -0600

    Added three slash command files and defined the commands

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js')
+const fs = require('fs')
+const path = require('path')
+
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
 const dotenv = require('dotenv')
 
 dotenv.config()
@@ -6,6 +9,8 @@ dotenv.config()
 const TOKEN = process.env.TOKEN
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.commands = new Collection()
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`)
